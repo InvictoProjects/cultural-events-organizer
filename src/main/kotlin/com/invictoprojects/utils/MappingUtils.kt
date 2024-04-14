0fe3f97b170d56f584d3c5906b7dc7ef2bcc6e23
Volodymyr Pienskoi <vladymyrpenskoy@gmail.com>
2024-04-14 20:19:06 +0300

    CEO-7: Update Event Controller

--- src/main/kotlin/com/invictoprojects/utils/MappingUtils.kt
+++ src/main/kotlin/com/invictoprojects/utils/MappingUtils.kt
@@ -1,6 +1,8 @@
 package com.invictoprojects.utils
 
+import com.invictoprojects.dto.EventDto
 import com.invictoprojects.dto.UserDto
+import com.invictoprojects.model.Event
 import com.invictoprojects.model.User
 
 object MappingUtils {
@@ -12,6 +14,18 @@ object MappingUtils {
             email = userDto.email,
             firstName = userDto.firstName,
             lastName = userDto.lastName
+        )
+    }
+
+    fun convertToEntity(eventDto: EventDto): Event {
+        return Event(
+            name = eventDto.name,
+            description = eventDto.description,
+            startTime = eventDto.startTime,
+            endTime = eventDto.endTime,
+            eventType = eventDto.eventType,
+            location = eventDto.location,
+            url = eventDto.url
         )
     }
 
