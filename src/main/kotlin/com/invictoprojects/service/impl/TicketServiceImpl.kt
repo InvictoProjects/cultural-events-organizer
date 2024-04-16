a2e93e26ad886d304e7d373a0719bf980b2c69e9
Vladyslav Holik <33317989+VladyslavHolik@users.noreply.github.com>
2024-04-16 19:01:37 +0200

    CEO-8 Adding Ticket service

---
+++ src/main/kotlin/com/invictoprojects/service/impl/TicketServiceImpl.kt
@@ -0,0 +1,37 @@
+package com.invictoprojects.service.impl
+
+import com.invictoprojects.model.Event
+import com.invictoprojects.model.Ticket
+import com.invictoprojects.model.User
+import com.invictoprojects.repository.TicketRepository
+import com.invictoprojects.service.TicketService
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import java.time.Instant
+import java.time.LocalDateTime
+
+
+@Singleton
+class TicketServiceImpl(
+    @Inject private val ticketRepository: TicketRepository
+) : TicketService {
+
+    override fun purchaseTickets(event: Event, user: User, amount: Long) {
+        if (event.endTime < LocalDateTime.now()) {
+            throw IllegalArgumentException("Event already ended")
+        }
+        for (i in 1..amount) {
+            val ticket = Ticket(event, user, Instant.now())
+            ticketRepository.save(ticket)
+        }
+    }
+
+    override fun findPurchasedTicketsByUserId(userId: Long): MutableIterable<Ticket> {
+        return ticketRepository.findByUserId(userId)
+    }
+
+    override fun deleteById(id: Long) {
+        ticketRepository.deleteById(id)
+    }
+
+}
