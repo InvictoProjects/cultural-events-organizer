986330f513de139d8100e051f2a35015364a8394
Volodymyr Pienskoi <vladymyrpenskoy@gmail.com>
2024-05-31 22:49:56 +0300

    CEO-24: Update Discount Code Service and Repository

--- event-management-service/src/main/kotlin/com/invictoprojects/service/DiscountCodeService.kt
+++ event-management-service/src/main/kotlin/com/invictoprojects/service/DiscountCodeService.kt
@@ -4,8 +4,13 @@ import com.invictoprojects.model.DiscountCode
 import com.invictoprojects.model.Event
 
 interface DiscountCodeService {
+
     fun createDiscountCode(event: Event, code: String, discountPercentage: Float): DiscountCode
+
     fun deleteDiscountCode(id: Long)
+
     fun findByCode(code: String): DiscountCode?
+
     fun findByEvent(event: Event): List<DiscountCode>
+
 }
